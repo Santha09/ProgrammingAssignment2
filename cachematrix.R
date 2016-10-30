0e16b410e2debf9eedad448918552f189373d20f
Santha Krishnasamy <santha.gokul@thinkleaf.org>
2016-10-30 15:30:14 -0600

    Created function as per the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function does the inverse of a matrix and cache the value to avoid repetitive processing.
+## if the matrix is same and the inverse of it is already done and the value is in the cache, then the cache value is returned.
 
-## Write a short comment describing this function
+## The makeCacheMatrix function, caches the input and the inverse output value.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  Invmat = NULL
+  setMatrix <- function(y){
+    x <<- y
+    Invmat <<- NULL
+  }
+  getMatrix <- function() x
+  setInvmat <- function(Invmatrix) Invmat <<- Invmatrix
+  getInvmat <- function() Invmat
+  list (setMatrix = setMatrix, getMatrix = getMatrix,
+          setInvmat = setInvmat, getInvmat = getInvmat)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve, checks if the inverse of the input already exists in cache and if so, 
+##it returns from cache, else it inverse the data, cache it and then return the value.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+     Invmat <- x$getInvmat()
+     if (!is.null(Invmat)){
+       message("getting cached matrix")
+       return(Invmat)
+     }
+     else {
+       matrixd <- x$getMatrix()
+       inverse <- solve(matrixd,...)
+       x$setInvmat(inverse)
+       Invmat <- x$getInvmat()
+       Invmat
+     }
 }
+
+
